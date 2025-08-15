d755dbde8bf205231af204a01856f08e89cf4ee8
AntoineMargo <103125443+AntoineMargo@users.noreply.github.com>
2025-08-15 22:11:48 +0200

    AI personality template

---
+++ entities/personality.gd
@@ -0,0 +1,13 @@
+extends Resource
+
+class_name Personality
+
+@export_range(1, 100) var aggression = 50
+@export_range(1, 100) var loyalty = 50
+@export_range(1, 100) var empathy = 50
+
+@export_range(1, 100) var ambition = 25
+@export_range(1, 100) var intelligence = 40
+@export_range(1, 100) var charisma = 40
+
+@export_range(1, 100) var morale = 50
