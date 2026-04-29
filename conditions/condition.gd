2c4f53d33ad098866e2f3462fc492f346f47879b
Antoine Margoloff <antoine.mrg@gmail.com>
2026-04-29 19:17:48 +0200

    Git test.

--- conditions/condition.gd
+++ conditions/condition.gd
@@ -17,6 +17,8 @@ var vfx_instance: Node = null
 var linked_items: Array[Item] = []
 var linked_modifiers: Array[ModifierEntry] = []
 
+var testvar
+
 var user = null
 var target = null
 var user_uid = null
